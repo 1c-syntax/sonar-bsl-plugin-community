e0f2b29ee771a7fe68a96cd4e9c16a8ae484b1c6
Nikita Gryzlov <nixel2007@gmail.com>
2019-01-02 16:41:08 +0700

    Обновление заголовка лицензии

--- src/main/java/com/github/_1c_syntax/sonar/bsl/package-info.java
+++ src/main/java/com/github/_1c_syntax/sonar/bsl/package-info.java
@@ -1,2 +1,23 @@
+/*
+ * This file is a part of SonarQube 1C (BSL) Community Plugin.
+ *
+ * Copyright © 2018-2019
+ * Nikita Gryzlov <nixel2007@gmail.com>
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * SonarQube 1C (BSL) Community Plugin is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * SonarQube 1C (BSL) Community Plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with SonarQube 1C (BSL) Community Plugin.
+ */
 @javax.annotation.ParametersAreNonnullByDefault
 package com.github._1c_syntax.sonar.bsl;
