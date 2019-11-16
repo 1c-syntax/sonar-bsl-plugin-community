e3e12a7e4d5efa3a182faeab71aaaf2011457c2c
Nikita Gryzlov <nixel2007@gmail.com>
2019-11-16 21:33:41 +0300

    upgrade bsl ls

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ repositories {
 dependencies {
     implementation("org.sonarsource.sonarqube:sonar-plugin-api:7.9")
 
-    compile("com.github.1c-syntax:bsl-language-server:ce1244b3e6e6b7039311e16398985ab71a4c9471")
+    compile("com.github.1c-syntax:bsl-language-server:0.12.0")
     compile("com.fasterxml.jackson.core:jackson-databind:2.10.0")
     compile("com.google.code.findbugs:jsr305:3.0.2")
     // https://mvnrepository.com/artifact/org.sonarsource.analyzer-commons/sonar-analyzer-commons
