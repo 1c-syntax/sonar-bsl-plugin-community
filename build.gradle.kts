4946ac595d0cdea6549f58a40a652aab9e93dadf
Nikita Gryzlov <nixel2007@gmail.com>
2019-11-29 16:32:05 +0300

    Подсчет метрики комментарии

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ repositories {
 dependencies {
     implementation("org.sonarsource.sonarqube:sonar-plugin-api:7.9")
 
-    compile("com.github.1c-syntax:bsl-language-server:2be990558a11097e8c525da361d04ad2abb06fbe")
+    compile("com.github.1c-syntax:bsl-language-server:7b97dbf040945cb25137a071ab58a347e3a75f76")
     compile("com.fasterxml.jackson.core:jackson-databind:2.10.0")
     compile("com.google.code.findbugs:jsr305:3.0.2")
     // https://mvnrepository.com/artifact/org.sonarsource.analyzer-commons/sonar-analyzer-commons
