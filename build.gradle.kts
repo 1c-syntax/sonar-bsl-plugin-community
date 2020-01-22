41217332a0bd306a283a247f95a190b5129f9367
Nikita Gryzlov <nixel2007@gmail.com>
2020-01-22 21:38:46 +0300

    bsl ls bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ repositories {
 dependencies {
     implementation("org.sonarsource.sonarqube:sonar-plugin-api:7.9")
 
-    compile("com.github.1c-syntax:bsl-language-server:3fcb46e4c1ec0ff044deed78fbba86be38ece38e")
+    compile("com.github.1c-syntax:bsl-language-server:v0.13.0-RC6")
     compile("com.fasterxml.jackson.core:jackson-databind:2.10.0")
     compile("com.google.code.findbugs:jsr305:3.0.2")
     // https://mvnrepository.com/artifact/org.sonarsource.analyzer-commons/sonar-analyzer-commons
