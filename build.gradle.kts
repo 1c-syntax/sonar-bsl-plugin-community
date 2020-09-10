e3c8e6af804ac10b7b99bc64ce3145aff13e1cdc
Nikita Gryzlov <nixel2007@gmail.com>
2020-09-10 13:30:05 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ val junitVersion = "5.6.1"
 dependencies {
     implementation("org.sonarsource.sonarqube:sonar-plugin-api:7.9")
 
-    implementation("com.github.1c-syntax", "bsl-language-server", "d17e900fb306361b888f1e7c305807345da56278")
+    implementation("com.github.1c-syntax", "bsl-language-server", "0.16.0-RC2")
 
     implementation("org.apache.commons:commons-lang3:3.10")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.10.3")
