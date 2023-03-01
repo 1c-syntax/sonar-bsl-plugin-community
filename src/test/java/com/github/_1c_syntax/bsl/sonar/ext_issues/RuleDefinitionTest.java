bc0121fda3745c4256b01921f2047c4c7ffbd9c8
Valery Maximov <valery.maximov@bia-tech.ru>
2023-03-01 15:10:54 +0300

    Обновлены правила для едт

--- src/test/java/com/github/_1c_syntax/bsl/sonar/ext_issues/RuleDefinitionTest.java
+++ src/test/java/com/github/_1c_syntax/bsl/sonar/ext_issues/RuleDefinitionTest.java
@@ -46,7 +46,7 @@ class RuleDefinitionTest {
     assertThat(context.repositories()).hasSize(1);
     var repository = context.repository(reporter.getRepositoryKey());
     assertThat(repository).isNotNull();
-    assertThat(repository.rules()).hasSize(118);
+    assertThat(repository.rules()).hasSize(179);
   }
 
   @Test
@@ -62,7 +62,7 @@ class RuleDefinitionTest {
     assertThat(context.repositories()).hasSize(1);
     var repository = context.repository(reporter.getRepositoryKey());
     assertThat(repository).isNotNull();
-    assertThat(repository.rules()).hasSize(118);
+    assertThat(repository.rules()).hasSize(179);
   }
 
   @Test
@@ -84,7 +84,7 @@ class RuleDefinitionTest {
     assertThat(context.repositories()).hasSize(1);
     var repository = context.repository(reporter.getRepositoryKey());
     assertThat(repository).isNotNull();
-    assertThat(repository.rules()).hasSize(122);
+    assertThat(repository.rules()).hasSize(183);
   }
 
 }
