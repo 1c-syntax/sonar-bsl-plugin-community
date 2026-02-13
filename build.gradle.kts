fd1ea3973ac566e13e81c3c92bf9fa7bca70d426
theshadowco <maximovvalery@gmail.com>
2026-02-13 13:22:54 +0300

    jdk 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,8 +52,8 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 tasks.withType<JavaCompile> {
