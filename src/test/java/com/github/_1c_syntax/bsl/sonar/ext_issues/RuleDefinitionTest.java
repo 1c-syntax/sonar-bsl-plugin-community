e9f8a374ea6c11ef53d82b09bae95285081ce3b0
Maximov Valery <maximovvalery@gmail.com>
2026-03-23 07:51:37 +0300

    Обновленный список правил EDT (#420)

--- src/test/java/com/github/_1c_syntax/bsl/sonar/ext_issues/RuleDefinitionTest.java
+++ src/test/java/com/github/_1c_syntax/bsl/sonar/ext_issues/RuleDefinitionTest.java
@@ -46,7 +46,7 @@ class RuleDefinitionTest {
     assertThat(context.repositories()).hasSize(1);
     var repository = context.repository(reporter.getRepositoryKey());
     assertThat(repository).isNotNull();
-    assertThat(repository.rules()).hasSize(281);
+    assertThat(repository.rules()).hasSize(309);
     assertThat(repository.rules()).allMatch(rule -> rule.name().length() < 200);
   }
 
@@ -63,7 +63,7 @@ class RuleDefinitionTest {
     assertThat(context.repositories()).hasSize(1);
     var repository = context.repository(reporter.getRepositoryKey());
     assertThat(repository).isNotNull();
-    assertThat(repository.rules()).hasSize(281);
+    assertThat(repository.rules()).hasSize(309);
     assertThat(repository.rules()).allMatch(rule -> rule.name().length() < 200);
   }
 
@@ -86,7 +86,7 @@ class RuleDefinitionTest {
     assertThat(context.repositories()).hasSize(1);
     var repository = context.repository(reporter.getRepositoryKey());
     assertThat(repository).isNotNull();
-    assertThat(repository.rules()).hasSize(285);
+    assertThat(repository.rules()).hasSize(313);
     assertThat(repository.rules()).allMatch(rule -> rule.name().length() < 200);
   }
 
@@ -109,7 +109,7 @@ class RuleDefinitionTest {
     assertThat(context.repositories()).hasSize(1);
     var repository = context.repository(reporter.getRepositoryKey());
     assertThat(repository).isNotNull();
-    assertThat(repository.rules()).hasSize(281);
+    assertThat(repository.rules()).hasSize(309);
     assertThat(repository.rules()).allMatch(rule -> rule.name().length() < 200);
   }
 }
