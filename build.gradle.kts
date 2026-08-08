4609b7d8cbe30d1975f8df8fe0fa1e300e0052a9
theshadowco <maximovvalery@gmail.com>
2026-08-08 20:47:08 +0300

    bump bslls

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ val commonmarkVersion = "0.29.0"
 dependencies {
     compileOnly("org.sonarsource.api.plugin:sonar-plugin-api:11.3.0.2824")
 
-    implementation("io.github.1c-syntax:bsl-language-server:1.0.6") {
+    implementation("io.github.1c-syntax:bsl-language-server:1.0.7") {
         exclude("com.contrastsecurity", "java-sarif")
         exclude("info.picocli", "picocli-spring-boot-starter")
         exclude("me.tongfei", "progressbar")
